b9f593458129b98e9330eabed8d8247c559eea0f
SSab <sabsay@ix.netcom.com>
2015-04-23 19:39:42 -0700

    Update sab-sms_3.0.php

--- sab-sms_3.0.php
+++ sab-sms_3.0.php
@@ -10,7 +10,8 @@
  $num = $_GET['num']; 
  $msg = $_GET['msg']; 
   
- $sub = "Hold ".$frm." to reply"; 
+ //$sub = "Hold ".$frm." to reply"; 
+ $sub = "From my Pebble";
  $frm = "pebble@sab-sms_3.0.php"; 
  $hdr = "From: ".$frm; 
 
